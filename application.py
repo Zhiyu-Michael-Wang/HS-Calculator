9dc484eaaa19c453ceda21fe9f7535ec2540557e
Zhiyu Wang <wzy66618@outlook.com>
2020-10-28 21:42:45 +0800

    first commit

---
+++ application.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,redirect,render_template,url_for,send_from_directory
+from markupsafe import escape
+import calculator as hsc
+
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return redirect(url_for('calculation'))
+
+
+@application.route('/calculation', methods=["POST", "GET"])
+def calculation():
+    if request.method == "POST":
+        winning_needed = round(float(request.form["total_winning_needed"]))
+        winning_star_bounce = round(float(request.form["winning_star_bounce"]))
+        rate = float(request.form["winning_rate"])
+        five_to_one = request.form["five_to_one"] == "Yes"
+
+        imd = hsc.run(
+            total_winning_needed=winning_needed,
+            star_bounce=winning_star_bounce,
+            winning_rate=rate,
+            have_five_to_one=five_to_one
+        )
+        
+        
+        return render_template('result.html', file_url=imd)
+    else:
+        return render_template('calculation.html')
+
+
+
+if __name__ == "__main__":
+    application.run()
+
+
+
